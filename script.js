66e477268674b380e1efffbd524f8cbe30111af0
neela-sankar <neela.ecest@gmail.com>
2024-08-22 22:24:43 +0800

    css and js updated

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
     labels.forEach(label => {
         label.addEventListener('click', () => {
-            // Get the current offer and its size-color div
+            
             const currentOffer = label.closest('.offer');
             const sizeColorDiv = currentOffer.querySelector('.size-color');
 
-            // Remove the 'selected' class from all offers
+            
             document.querySelectorAll('.offer').forEach(offer => {
                 offer.classList.remove('selected');
                 offer.querySelector('.size-color').style.display = 'none'; // Hide all size-color divs
             });
 
-            // Add the 'selected' class to the clicked offer
+            
             currentOffer.classList.add('selected');
 
             // Toggle the display of the clicked offer's size-color div
